1384be91e1d4a8d76858822c79449c9c31e12f13
kaho.tsujine <tsuzyshoo@gmail.com>
2020-06-03 12:46:26 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -48,6 +48,18 @@ class UsersController < ApplicationController
     
     flash[:success] = '退会しました'
     redirect_to root_url
+  end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
   
   private
